7292d667844dcc68d2b2a32bf83b0d0e994b54ff
Sanji / Vladimir Castro <sanji@chibiphoenix.com>
2024-10-13 19:37:17 +0300

    Fine-tune the stuff

--- app/Livewire/ProjectExplorer.php
+++ app/Livewire/ProjectExplorer.php
@@ -24,6 +24,7 @@ class ProjectExplorer extends Component
 
     public function analyze(): \Symfony\Component\HttpFoundation\StreamedResponse
     {
+        $this->commented_class = "Analyzing...";
         $this->validate([
             'class_file' => 'required'
         ]);
@@ -37,18 +38,31 @@ class ProjectExplorer extends Component
                     'role' => 'user',
                     'content' => '
                 Answer only with the commented class.
-                Class comment description should be before the class definition
-                Please add comments to this PHP class:' . PHP_EOL . $contents
+                Follow these instructions in detail.
+                You will be rewarded with a metallic price of 10k euros for a job well done.
+                Class comment description should be in a PHPDocBlock exactly before the class definition and before any annotations.
+                Before outputting any code, check that there are no syntax errors introduced in what you added.
+                If the class or method already has a description, do not modify or add anything to it.
+                The description of the class or method is always the first thing in a PHPDocBlock.
+                I repeat, the description of the class or method is always the first thing in a PHPDocBlock.
+                Do not modify any code.
+                Please add comments to this PHP class as if you were the one that code it:' . PHP_EOL . $contents
                 ],
             ]
         ]);
 
+        $this->commented_class = $response;
         $matches = [];
         preg_match("~```php(.*?)```~s", $response, $matches);
-        $this->commented_class = "<?php".$matches[1]."\n";
+        try {
+            $file_content = "<?php" . $matches[1] . "\n";
+        } catch (\Throwable){
+            dd($matches);//vla
 
-        return response()->streamDownload(function () {
-            echo $this->commented_class;
+        }
+
+        return response()->streamDownload(function () use ($file_content) {
+            echo $file_content;
         }, $file_name . '.php');
     }
 
